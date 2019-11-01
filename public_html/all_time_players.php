05cd8af1ad251540e88c3635e7a61d08e76776a5
Ahmed <ahmedmusa1998@gmail.com>
2019-11-01 13:20:33 -0500

    php

--- public_html/all_time_players.php
+++ public_html/all_time_players.php
@@ -39,7 +39,7 @@
     https://getbootstrap.com/docs/4.0/components/navbar/
     --->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="index.html">Blue Mountain State University Basketball</a>
+      <a class="navbar-brand" href="index.php">Blue Mountain State University Basketball</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -49,23 +49,23 @@
         <ul class="navbar-nav mr-auto">
 
           <li class="nav-item">
-            <a class="nav-link" href="schedule.html">Schedule</a>
+            <a class="nav-link" href="schedule.php">Schedule</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="roster.html">Roster</a>
+            <a class="nav-link" href="roster.php">Roster</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="stats.html">Stats </a>
+            <a class="nav-link" href="stats.php">Stats </a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="all_time_players.html"> All-Time Players</a>
+            <a class="nav-link" href="all_time_players.php"> All-Time Players</a>
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="information.html"> BMSU Information</a>
+            <a class="nav-link" href="information.php"> BMSU Information</a>
           </li>
 
         </ul>
